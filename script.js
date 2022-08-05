2d38358e424104c2a9ab3de5205e58e2c9047f32
DMon <davemonaghan@gmx.com>
2022-08-05 20:01:05 +0100

    html & css updated

---
+++ script.js
@@ -0,0 +1,16 @@
+var searchButton = document.getElementById("search-button")
+var userInput = document.getElementById("user-input")
+
+
+
+
+
+
+
+searchCity ()
+
+searchButton.addEventListener("click" searchCity)
+
+
+
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={27c15d159618d2da5af0904826c56b48}
