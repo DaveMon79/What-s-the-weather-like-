262216f7194a0fd7d4390d8703cdf4cf28bea4db
DMon <davemonaghan@gmx.com>
2022-08-06 10:25:59 +0100

    COR's error

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 var searchButton = document.getElementById("search-button")
-var userInput = document.getElementById("user-input")
+var cityName = document.getElementById("user-input")
+var apiKey = "27c15d159618d2da5af0904826c56b48"
+var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+function searchCity(requestUrl) {
+    fetch(requestUrl)
+        .then(function (response) {
+            console.log(response);
+            if (response.status === 404) 
+                console.log("error 404")
+
+            
+            return response.json();
+
+        })
+}
+
+
+
+searchButton.addEventListener("click", searchCity)
 
 
 
 
 
+// https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=27c15d159618d2da5af0904826c56b48
 
 
-searchCity ()
-
-searchButton.addEventListener("click" searchCity)
-
-
-
-//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={27c15d159618d2da5af0904826c56b48}
+// 'https://api.openweathermap.org/data/2.5/weather?q=manchester&appid=27c15d159618d2da5af0904826c56b48'
